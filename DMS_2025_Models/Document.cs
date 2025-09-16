2bff0be23aa8293da3a1de0b45e13fe81206fed1
Andrei Brate <andrei.brate@gmail.com>
2025-09-16 12:34:39 +0200

    Fixed typo in project name

---
+++ DMS_2025_Models/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS_2025_Models
+{
+    public class Document
+    {
+        [Required]
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Location { get; set; }
+
+        // metadata
+        public TimeSpan? CreationDate { get; set; }
+        public string? Author { get; set; }
+    }
+}
