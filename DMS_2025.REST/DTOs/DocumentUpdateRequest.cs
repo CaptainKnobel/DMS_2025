5ff0b82e1f1a8f5f9009fa7c40ed532d99d5a889
CaptainKnobel <cklein@live.at>
2025-09-17 14:58:53 +0200

    first version release, probably won't work, needs merge with DAL

---
+++ DMS_2025.REST/DTOs/DocumentUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMS_2025.REST.DTOs
+{
+    public class DocumentUpdateRequest
+    {
+        [MaxLength(255)]
+        public string? Title { get; set; }
+        [MaxLength(1024)]
+        public string? Tags { get; set; }
+    }
+}
--- DMS_2025.REST/Program.cs
+++ DMS_2025.REST/Program.cs
@@ -1,11 +1,26 @@
+//using System.Reflection;
+//using System.Text.Json;
+//using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+//using Serilog;
+using DMS_2025.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// - Logging
+//builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddProblemDetails(); //n
+//builder.Services.AddValidatorsFromAssemblyContaining<DocumentCreateValidator>();
+
+// DAL (DbContext + Repos, incl. ConnectionString)
+builder.Services.AddDal(builder.Configuration);
 
 var app = builder.Build();
 
@@ -16,10 +31,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler(); // exception handler, because you alone can't handle my exceptional programming skills
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy" })); // i'm sick rn so it'd be good if at least one of us is healthy ...
 
 app.Run();
